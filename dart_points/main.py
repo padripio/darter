6dab8c77b2a9f3e01e3214392e5f322526b27040
Padre Pio <144624630+padripio@users.noreply.github.com>
2024-05-29 20:50:40 +0300

    Add files via upload

--- dart_points/main.py
+++ dart_points/main.py
@@ -2,7 +2,11 @@
 import datetime
 import tkinter
 from tkinter import messagebox
-BACKGROUND_COLOR="#5e6b64"
+
+FONT = ("Helvetica", "12",)
+POINTS_FONT = FONT
+
+BACKGROUND_COLOR = "#5e6b64"
 import time
 
 # clearing previous logs
@@ -11,19 +15,31 @@ time_now = datetime.datetime.now()
 hour = time_now.hour
 minute = time_now.minute
 game_mode = 501
-
-with open(file="game_logs.txt", mode="w") as file:
-    file.write(f"{hour} : {minute}\n")
-
-with open(file="player_names.txt", mode="r") as file:
-    names = file.readline()
-
-    name1 = names.split(sep=",")[0]
-    name2 = names.split(sep=",")[1]
+global game_done
+game_done = 0
 
 
 # adding a welcome screen
 def main(round, game_mode, default_points, index):
+    global player_points
+    player_points = 501
+    with open(file="game_logs.txt", mode="w") as file:
+        file.write(f"{hour} : {minute}\n")
+
+    with open(file="player_names.txt", mode="r") as file:
+        names = file.readline()
+
+        name1 = names.split(sep=",")[0]
+        name2 = names.split(sep=",")[1]
+    try:
+        with open(file="game_mode.txt", mode="r") as file:
+            text = file.read()
+
+        game_mode = int(text)
+        player_points = game_mode
+        print(f"game mode : {game_mode}")
+    except:
+        pass
     global round_count
     round_count = round
     global points1
@@ -43,80 +59,87 @@ def main(round, game_mode, default_points, index):
     window = tkinter.Tk()
     window.title("Dart points")
     window.minsize(width=300, height=300)
-    window.config(bg=BACKGROUND_COLOR,)
+    window.config(bg=BACKGROUND_COLOR, )
 
     player1_name = name1
     player2_name = name2
 
     # set up the canvas
-    title_text = tkinter.Label(bg=BACKGROUND_COLOR,text="Dart points", )
-    game_mode_label = tkinter.Label(bg=BACKGROUND_COLOR,text=f"Game : {game_mode}", )
-    player1_label = tkinter.Label(bg=BACKGROUND_COLOR,text=player1_name, )
-    player2_label = tkinter.Label(bg=BACKGROUND_COLOR,text=player2_name, )
+    title_text = tkinter.Label(bg=BACKGROUND_COLOR, text="Dart points", )
+    game_mode_label = tkinter.Label(bg=BACKGROUND_COLOR, text=f"Game : {game_mode}", )
+    player1_label = tkinter.Label(bg=BACKGROUND_COLOR, text=player1_name, font=FONT)
+    player2_label = tkinter.Label(bg=BACKGROUND_COLOR, text=player2_name, font=FONT)
 
     # TODO fetch the game mode from welcome and bind here
 
-    player1_points = tkinter.Label(bg=BACKGROUND_COLOR,text=points1,)
-    player2_points = tkinter.Label(bg=BACKGROUND_COLOR,text=points2, )
-    entry_text = tkinter.Label(bg=BACKGROUND_COLOR,text="enter points here")
+    player1_points = tkinter.Label(bg=BACKGROUND_COLOR, text=text, font=POINTS_FONT)
+    player2_points = tkinter.Label(bg=BACKGROUND_COLOR, text=text, font=POINTS_FONT)
+    entry_text = tkinter.Label(bg=BACKGROUND_COLOR, text="enter points here", font=("Helvetica", "12",))
 
     player1_entry = tkinter.Entry()
     player2_entry = tkinter.Entry()
-    enter_button_1 = tkinter.Button(text="Submit")
+    enter_button_1 = tkinter.Button(text="Enter", font=FONT)
     enter_button_2 = tkinter.Button(text="enter")
-    new_game = tkinter.Button(text="Start new game", bg=BG_COLOR)
+    new_game = tkinter.Button(text="Start new game", bg=BG_COLOR, font=FONT)
     change_game = tkinter.Button(text=f"Change game to {alt_game_mode(game_mode)}", bg=BG_COLOR)
 
     # packing the elements
-    title_text.grid(row=0, column=1)
-    game_mode_label.grid(row=0, column=2)
+    #title_text.grid(row=0, column=1)
+    game_mode_label.grid(row=0, column=0,columnspan=2,sticky="e")
 
-    space_text = tkinter.Label(text=" ", bg=BACKGROUND_COLOR,)
+    space_text = tkinter.Label(text=" ", bg=BACKGROUND_COLOR, )
     space_text.grid(row=1, column=0)
 
     player1_label.grid(row=2, column=0)
     player2_label.grid(row=2, column=2)
 
-    space_text = tkinter.Label(text=" ",bg=BACKGROUND_COLOR,)
+    space_text = tkinter.Label(text=" ", bg=BACKGROUND_COLOR, )
     space_text.grid(row=3, column=0)
 
     player1_points.grid(row=4, column=0)
     player2_points.grid(row=4, column=2)
 
-    space_text = tkinter.Label(text=" ",bg=BACKGROUND_COLOR,)
+    space_text = tkinter.Label(text=" ", bg=BACKGROUND_COLOR, )
     space_text.grid(row=5, column=0)
 
     entry_text.grid(row=6, column=1, columnspan=1)
 
-    space_text = tkinter.Label(bg=BACKGROUND_COLOR,text=" ", )
+    space_text = tkinter.Label(bg=BACKGROUND_COLOR, text=" ", )
     space_text.grid(row=7, column=0)
 
     player1_entry.grid(row=8, column=0)
     player2_entry.grid(row=8, column=2)
 
-    space_text = tkinter.Label(bg=BACKGROUND_COLOR,text=" ", )
+    space_text = tkinter.Label(bg=BACKGROUND_COLOR, text=" ", )
     space_text.grid(row=9, column=0)
 
     enter_button_1.grid(row=10, column=1)
     # enter_button_2.grid(row=10,column=2)
 
-    space_text = tkinter.Label(bg=BACKGROUND_COLOR,text=" ", )
+    space_text = tkinter.Label(bg=BACKGROUND_COLOR, text=" ", )
     space_text.grid(row=11, column=0)
 
     new_game.grid(row=12, column=1, columnspan=1)
 
-    space_text = tkinter.Label(text=" ", bg=BACKGROUND_COLOR,)
+    space_text = tkinter.Label(text=" ", bg=BACKGROUND_COLOR, )
     space_text.grid(row=13, column=1)
 
-    change_game.grid(row=14, column=1, columnspan=1)
+    # change_game.grid(row=14, column=1, columnspan=1)
     player1_entry.focus()
 
     def points_minus(points, current_points):
         current_points -= points
         return current_points
 
+    def empty_command():
+        # for button freezing
+        global game_done
+        game_done = 1
+
     def show_winner(winner):
         messagebox.showinfo(title="Hongera", message=f"{winner} wins !")
+        empty_command()
+        enter_button_1.config(command=empty_command)
 
     def shift_focus(event):
         # move the focus to th p2 bar
@@ -140,65 +163,74 @@ def main(round, game_mode, default_points, index):
 
     # todo fetch the game mode from welcome and bind here
 
+    points1 = player_points
+    points2 = player_points
+
     def deductor(p1, p2):
-        global points1, points2
+        global points1
+        global points2
+        global game_done
 
-        # enter_button_1.config(bg=FLASH_COLOR)
-        # time.sleep(1)
-        # enter_button_2.config(bg=BG_COLOR)
-        # empty_fields=0
-        # p1 = player1_entry.get()
-        # p2 = player2_entry.get()
-        # if len(p1) or len(p2)<1:
-        #    messagebox.showerror(title="empty fields" ,message="Please fill in all the fields")
-        #
+        if game_done == 1:
+            None
+        else:
 
-        try:
-            p1 = int(p1)
-            p2 = int(p2)
+            # enter_button_1.config(bg=FLASH_COLOR)
+            # time.sleep(1)
+            # enter_button_2.config(bg=BG_COLOR)
+            # empty_fields=0
+            # p1 = player1_entry.get()
+            # p2 = player2_entry.get()
+            # if len(p1) or len(p2)<1:
+            #    messagebox.showerror(title="empty fields" ,message="Please fill in all the fields")
+            #
 
-            # for player1
+            try:
+                p1 = int(p1)
+                p2 = int(p2)
 
-            new_point = points1
-            new_point -= p1
-            if new_point < 0:
-                print("Miss")
-            elif new_point == 0:
-                show_winner(player1_name)
+                # for player1
 
-                points1 = new_point
-            else:
-                points1 = new_point
+                new_point = points1
+                new_point -= p1
+                if new_point < 0:
+                    None
+                elif new_point == 0:
+                    show_winner(player1_name)
 
-            # TODO for player2
+                    points1 = new_point
+                else:
+                    points1 = new_point
 
-            new_point = points2
-            new_point -= p2
-            if new_point < 0:
-                print("Miss")
-            elif new_point == 0:
-                show_winner(player2_name)
-                points2 = new_point
-            else:
-                points2 = new_point
+                # TODO for player2
 
-            player1_points.config(text=points1)
-            player2_points.config(text=points2)
+                new_point = points2
+                new_point -= p2
+                if new_point < 0:
+                    None
+                elif new_point == 0:
+                    show_winner(player2_name)
+                    points2 = new_point
+                else:
+                    points2 = new_point
 
-            # clearing the fields
-            player1_entry.delete(0, tkinter.END)
-            player2_entry.delete(0, tkinter.END)
+                player1_points.config(text=points1)
+                player2_points.config(text=points2)
 
-            global round_count
-            round_count += 1
-            print(f" Round count : {round_count}")
-            player1_entry.focus()
-            # TODO game logs
-            with open(file="game_logs.txt", mode="a") as file:
-                file.write(f"\nRound : {round_count} \n{player1_name}->{p1} , {player2_name}->{p2}\n")
+                # clearing the fields
+                player1_entry.delete(0, tkinter.END)
+                player2_entry.delete(0, tkinter.END)
 
-        except ValueError:
-            messagebox.showerror(title="Invalid input", message="Please enter only numbers")
+                global round_count
+                round_count += 1
+                #print(f" Round count : {round_count}")
+                player1_entry.focus()
+                # TODO game logs
+                with open(file="game_logs.txt", mode="a") as file:
+                    file.write(f"\nRound : {round_count} \n{player1_name}->{p1} , {player2_name}->{p2}\n")
+
+            except ValueError:
+                messagebox.showerror(title="Invalid input", message="Please enter only numbers")
 
     def start_new_game():
         global points2
@@ -235,7 +267,8 @@ def main(round, game_mode, default_points, index):
                     player2_points.config(text=points2)
                     player1_points.config(text=points1)
                     change_game.config(text=f"Change game to {alt_game_mode(game_mode)}")
-                    return game_mode
+                    global player_points
+                    player_points = game_mode
 
     new_game.config(command=start_new_game)
 
@@ -246,5 +279,9 @@ def main(round, game_mode, default_points, index):
     window.mainloop()
 
 
+def main_caller():
+    main(0, 501, [501, 301], 0)
+
+
 if __name__ == "__main__":
-    main(0, 501, [501,301], 0)
+    main_caller()
--- dart_points/welcome.py
+++ dart_points/welcome.py
@@ -1,26 +1,78 @@
 import tkinter
+from main import main_caller
+
+FONT = ("Helvetica", "12",)
+FONT_NAME = "Courier"
+HIGHLIGHT_COLOR = "#f2dcc9"
 
 welcome = tkinter.Tk()
 welcome.config(background="#5e6b64")
 welcome.title("Darter")
 
-canvas = tkinter.Canvas(width=300, height=50)
+canvas = tkinter.Canvas(width=300, height=80)
 canvas.config(background="#5e6b64", highlightthickness=0)
 
 game_type_label = tkinter.Label(background="#5e6b64", text="Game type")
 
-button_501 = tkinter.Button(text="501 Game", background="#f2dcc9")
+button_501 = tkinter.Button(text="501 Game", background=HIGHLIGHT_COLOR)
 button_301 = tkinter.Button(text="301 Game")
 
-name1_label = tkinter.Label(background="#5e6b64", text="Player 1 name ")
-name2_label = tkinter.Label(background="#5e6b64", text="Player 2 name ")
+game_mode = 0
+
+# loading the icon
+img = tkinter.PhotoImage(file="arrow.ppm")
+# canvas.create_image(150, 10, image=img)
+darter_heading = canvas.create_text(150,40,text="DARTER", font=(FONT_NAME, 40, "bold"))
+
+# setting the default game state
+with open(file="game_mode.txt",mode="w") as file:
+    file.write(str(501))
+
+
+# binding the start button
+def activator():
+    main_caller()
+
+
+# creating a game mode file
+
+def game_mode_501():
+    # set the highlight
+    button_301.config(background="white", activebackground="white")
+    button_501.config(background=HIGHLIGHT_COLOR, fg="#000000", activebackground=HIGHLIGHT_COLOR,
+                      activeforeground="#000000")
+    global game_mode
+    game_mode = 501
+    with open(file="game_mode.txt", mode="w") as file:
+        game_mode = str(game_mode)
+        file.write(game_mode)
+
+    return game_mode
+
+
+def game_mode_301():
+    button_301.config(background=HIGHLIGHT_COLOR, fg="#000000", activebackground=HIGHLIGHT_COLOR,
+                      activeforeground="#000000")
+    button_501.config(background="white", activebackground="white")
+    global game_mode
+    game_mode = 301
+    with open(file="game_mode.txt", mode="w") as file:
+        game_mode = str(game_mode)
+        file.write(game_mode)
+    return game_mode
+
+
+button_301.config(command=game_mode_301)
+button_501.config(command=game_mode_501)
+
+name1_label = tkinter.Label(background="#5e6b64", text="Player 1 ", font=FONT)
+name2_label = tkinter.Label(background="#5e6b64", text="Player 2 ", font=FONT)
 name1_entry = tkinter.Entry()
 name2_entry = tkinter.Entry()
 name1_entry.focus()
 
 
 # shifting the focus
-
 
 
 # clean the name logs
@@ -39,6 +91,9 @@ def write_names():
     print(names_string)
     with open(file="player_names.txt", mode="w") as file:
         file.write(names_string)
+    # close the window
+    welcome.destroy()
+    activator()
 
 
 def shift_focus(event):
@@ -57,7 +112,6 @@ def shift_focus(event):
 
 welcome.bind("<Return>", shift_focus)
 
-
 # or_label = tkinter.Label(text="Or ")
 
 # default_name_button = tkinter.Button(text="Use default names")
@@ -67,10 +121,10 @@ start_button = tkinter.Button(text="Start", command=write_names)
 # packing the elements
 canvas.grid(row=0, column=0, columnspan=2)
 
-game_type_label.grid(row=1, column=0, padx=10, pady=10)
+# game_type_label.grid(row=1, column=0, padx=10, pady=10)
 
-button_501.grid(row=2, column=0, padx=5, pady=5)
-button_301.grid(row=3, column=0, padx=5, pady=5)
+button_501.grid(row=2, column=0, padx=5, pady=10)
+button_301.grid(row=3, column=0, padx=5, pady=10)
 
 name1_label.grid(row=4, column=0, padx=20, pady=10)
 name1_entry.grid(row=5, column=0, padx=20, pady=10)
